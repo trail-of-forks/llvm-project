dbf8f6953935fba35a854013602e5e7cb774076d
Peter Goodman <peter@trailofbits.com>
2023-09-23 22:44:39 -0400

    More annotation stuff.

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -4347,6 +4347,11 @@ static void handleAnnotateAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
         return;
       }
     }
+
+    if (!L.getNumArgs()) {
+      handleUnknownAttrAsAnnotateAttr(S, D, AL);
+      return;
+    }
   }
 
   // Make sure that there is a string literal as the annotation's first
