3a8d60a8da19b923a86a8b2816c084c34e8e715d
James Olds <james.olds@trailofbits.com>
2023-10-18 21:07:00 -0400

    support pasta tag

--- .github/workflows/set-release-binary-outputs.sh
+++ .github/workflows/set-release-binary-outputs.sh
@@ -12,11 +12,12 @@ github_user=$1
 tag=$2
 upload=$3
 
-if [[ "$github_user" != "tstellar" && "$github_user" != "tru" ]]; then
-  echo "ERROR: User not allowed: $github_user"
-  exit 1
-fi
-pattern='^llvmorg-[0-9]\+\.[0-9]\+\.[0-9]\+\(-rc[0-9]\+\)\?$'
+# if [[ "$github_user" != "tstellar" && "$github_user" != "tru" ]]; then
+#   echo "ERROR: User not allowed: $github_user"
+#   exit 1
+# fi
+
+pattern='^llvmorg-[0-9]\+\.[0-9]\+\.[0-9]\+\(-rc[0-9]\+\)\?\(-pasta\)\?$'
 echo "$tag" | grep -e $pattern
 if [ $? != 0 ]; then
   echo "ERROR: Tag '$tag' doesn't match pattern: $pattern"
@@ -25,7 +26,7 @@ fi
 release_version=`echo "$tag" | sed 's/llvmorg-//g'`
 release=`echo "$release_version" | sed 's/-.*//g'`
 build_dir=`echo "$release_version" | sed 's,^[^-]\+,final,' | sed 's,[^-]\+-rc\(.\+\),rc\1,'`
-rc_flags=`echo "$release_version" | sed 's,^[^-]\+,-final,' | sed 's,[^-]\+-rc\(.\+\),-rc \1 -test-asserts,' | sed 's,--,-,'`
+rc_flags=`echo "$release_version" | sed 's,^[^-]\+,-final,' | sed 's,[^-]\+-rc\(.\+\),-rc \1 -test-asserts,' | sed 's,--,-,' | sed 's,-pasta,,g'`
 echo "release-version=$release_version" >> $GITHUB_OUTPUT
 echo "release=$release" >> $GITHUB_OUTPUT
 echo "build-dir=$build_dir" >> $GITHUB_OUTPUT
