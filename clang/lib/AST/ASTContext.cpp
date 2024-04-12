2ab8edb951bc63d31695c6c1b9b0c5231ffb5e7f
AkshayK <iit.akshay@gmail.com>
2024-04-12 14:01:28 -0400

    check for duplicate types and fix the type checks before initialization

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -111,6 +111,23 @@ enum FloatingRank {
   Ibm128Rank
 };
 
+// Note(kumarak): Utility function to check if the underlying
+//                RecordDecl is same. If they are same, then
+//                it is duplicate type.
+namespace clang {
+bool
+isDuplicateType(const clang::Type *Ty1, const clang::Type *Ty2) {
+  if (Ty1 && Ty2 && Ty1->isRecordType() && Ty2->isRecordType()) {
+    return Ty1->getAsRecordDecl() == Ty2->getAsRecordDecl();
+  }
+
+  // TODO (kumark): Function ony check duplicate for RecordType
+  //                Should it be extended to other types as well??
+  return false;
+}
+} //namespace clang
+
+
 /// \returns location that is relevant when searching for Doc comments related
 /// to \p D.
 static SourceLocation getDeclLocForCommentSearch(const Decl *D,
