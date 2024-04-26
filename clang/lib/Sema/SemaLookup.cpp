4830f7036f584caca3a0a307bf8959832ae47291
AkshayK <iit.akshay@gmail.com>
2024-04-26 09:20:50 -0400

    Revert "Update checks for complete definition before qualified name lookup"
    This reverts commit d9d558ded6f72b1ee85b17b6808136f9fd8fa81f.

--- clang/lib/Sema/SemaLookup.cpp
+++ clang/lib/Sema/SemaLookup.cpp
@@ -2415,17 +2415,6 @@ bool Sema::LookupQualifiedName(LookupResult &R, DeclContext *LookupCtx,
   if (!R.getLookupName())
     return false;
 
-  // NOTE: The check here makes sure we are looking into the definition
-  //       of a CXXRecordDecl instead of declaration node not having
-  //       complete definition.
-  if (getLangOpts().LexicalTemplateInstantiation) {
-    if (isa<CXXRecordDecl>(LookupCtx) &&
-        !cast<CXXRecordDecl>(LookupCtx)->isCompleteDefinition() &&
-        cast<CXXRecordDecl>(LookupCtx)->hasDefinition()) {
-      LookupCtx = cast<CXXRecordDecl>(LookupCtx)->getDefinition();
-    }
-  }
-
   // Make sure that the declaration context is complete.
   assert((!isa<TagDecl>(LookupCtx) ||
           LookupCtx->isDependentContext() ||
