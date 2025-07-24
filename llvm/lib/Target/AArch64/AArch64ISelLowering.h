db5cc00b8a45729024e3787d8b19626f46b36cd0
Francesco Bertolaccini <francesco.bertolaccini@trailofbits.com>
2025-07-24 10:38:23 +0200

    [CT] Implement CTSELECT for AArch64 on scalars

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -96,6 +96,8 @@ enum NodeType : unsigned {
   CSNEG, // Conditional select negate.
   CSINC, // Conditional select increment.
 
+  CTSELECT, // AArch64 Constant-time conditional select, implemented with CSEL
+
   // Pointer to the thread's local storage area. Materialised from TPIDR_EL0 on
   // ELF.
   THREAD_POINTER,
@@ -1145,6 +1147,7 @@ private:
   SDValue LowerSELECT_CC(ISD::CondCode CC, SDValue LHS, SDValue RHS,
                          SDValue TVal, SDValue FVal, const SDLoc &dl,
                          SelectionDAG &DAG) const;
+  SDValue LowerCTSELECT(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerINIT_TRAMPOLINE(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerADJUST_TRAMPOLINE(SDValue Op, SelectionDAG &DAG) const;
   SDValue LowerJumpTable(SDValue Op, SelectionDAG &DAG) const;
@@ -1362,6 +1365,10 @@ private:
 
   bool shouldScalarizeBinop(SDValue VecOp) const override {
     return VecOp.getOpcode() == ISD::SETCC;
+  }
+
+  bool isSelectSupported(SelectSupportKind Kind) const override {
+    return true;
   }
 };
 
