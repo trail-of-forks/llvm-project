2a3a91e02c2d00e517a4bdf1e2c96f1f0344ebe4
AkshayK <iit.akshay@gmail.com>
2026-07-16 21:38:38 -0400

    [ConstantTime] Apply review fixes for llvm.ct.select core
    - Verifier: reject aggregate types instead of crashing ISel; add test.
    - Fix scalable-vector expansion crash when the element type is not a
      legal scalar; add RVV test.
    - LangRef: document poison/undef, noundef, permitted folds, and FMF
      behavior; rewrite Syntax/Overview.
    - Deduplicate CT_SELECT type-legalizer handlers into the SELECT paths.
    - Misc: FP int-twin assert, dead null check, in-place memory blend,
      flag-free PromoteNode, GlobalISel fallback test, comment cleanups.

--- llvm/lib/CodeGen/SelectionDAG/LegalizeFloatTypes.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeFloatTypes.cpp
@@ -160,8 +160,8 @@ void DAGTypeLegalizer::SoftenFloatResult(SDNode *N, unsigned ResNo) {
     case ISD::LOAD:        R = SoftenFloatRes_LOAD(N); break;
     case ISD::ATOMIC_LOAD: R = SoftenFloatRes_ATOMIC_LOAD(N); break;
     case ISD::ATOMIC_SWAP: R = BitcastToInt_ATOMIC_SWAP(N); break;
-    case ISD::SELECT:      R = SoftenFloatRes_SELECT(N); break;
-    case ISD::CT_SELECT:   R = SoftenFloatRes_CT_SELECT(N); break;
+    case ISD::SELECT:
+    case ISD::CT_SELECT:   R = SoftenFloatRes_SELECT(N); break;
     case ISD::SELECT_CC:   R = SoftenFloatRes_SELECT_CC(N); break;
     case ISD::FREEZE:      R = SoftenFloatRes_FREEZE(N); break;
     case ISD::STRICT_SINT_TO_FP:
@@ -962,15 +962,8 @@ SDValue DAGTypeLegalizer::SoftenFloatRes_ATOMIC_LOAD(SDNode *N) {
 SDValue DAGTypeLegalizer::SoftenFloatRes_SELECT(SDNode *N) {
   SDValue LHS = GetSoftenedFloat(N->getOperand(1));
   SDValue RHS = GetSoftenedFloat(N->getOperand(2));
-  return DAG.getSelect(SDLoc(N),
-                       LHS.getValueType(), N->getOperand(0), LHS, RHS);
-}
-
-SDValue DAGTypeLegalizer::SoftenFloatRes_CT_SELECT(SDNode *N) {
-  SDValue LHS = GetSoftenedFloat(N->getOperand(1));
-  SDValue RHS = GetSoftenedFloat(N->getOperand(2));
-  return DAG.getCTSelect(SDLoc(N), LHS.getValueType(), N->getOperand(0), LHS,
-                         RHS);
+  return DAG.getNode(N->getOpcode(), SDLoc(N), LHS.getValueType(),
+                     N->getOperand(0), LHS, RHS);
 }
 
 SDValue DAGTypeLegalizer::SoftenFloatRes_SELECT_CC(SDNode *N) {
@@ -1535,8 +1528,8 @@ void DAGTypeLegalizer::ExpandFloatResult(SDNode *N, unsigned ResNo) {
     // clang-format off
   case ISD::POISON:
   case ISD::UNDEF:        SplitRes_UNDEF(N, Lo, Hi); break;
-  case ISD::SELECT:       SplitRes_Select(N, Lo, Hi); break;
-  case ISD::CT_SELECT:    SplitRes_CT_SELECT(N, Lo, Hi); break;
+  case ISD::SELECT:
+  case ISD::CT_SELECT:    SplitRes_Select(N, Lo, Hi); break;
   case ISD::SELECT_CC:    SplitRes_SELECT_CC(N, Lo, Hi); break;
 
   case ISD::MERGE_VALUES:       ExpandRes_MERGE_VALUES(N, ResNo, Lo, Hi); break;
@@ -2588,9 +2581,9 @@ void DAGTypeLegalizer::SoftPromoteHalfResult(SDNode *N, unsigned ResNo) {
   case ISD::ATOMIC_LOAD:
     R = SoftPromoteHalfRes_ATOMIC_LOAD(N);
     break;
-  case ISD::SELECT:      R = SoftPromoteHalfRes_SELECT(N); break;
+  case ISD::SELECT:
   case ISD::CT_SELECT:
-    R = SoftPromoteHalfRes_CT_SELECT(N);
+    R = SoftPromoteHalfRes_SELECT(N);
     break;
   case ISD::SELECT_CC:   R = SoftPromoteHalfRes_SELECT_CC(N); break;
   case ISD::STRICT_SINT_TO_FP:
@@ -2857,15 +2850,8 @@ SDValue DAGTypeLegalizer::SoftPromoteHalfRes_ATOMIC_LOAD(SDNode *N) {
 SDValue DAGTypeLegalizer::SoftPromoteHalfRes_SELECT(SDNode *N) {
   SDValue Op1 = GetSoftPromotedHalf(N->getOperand(1));
   SDValue Op2 = GetSoftPromotedHalf(N->getOperand(2));
-  return DAG.getSelect(SDLoc(N), Op1.getValueType(), N->getOperand(0), Op1, Op2,
-                       N->getFlags());
-}
-
-SDValue DAGTypeLegalizer::SoftPromoteHalfRes_CT_SELECT(SDNode *N) {
-  SDValue Op1 = GetSoftPromotedHalf(N->getOperand(1));
-  SDValue Op2 = GetSoftPromotedHalf(N->getOperand(2));
-  return DAG.getCTSelect(SDLoc(N), Op1.getValueType(), N->getOperand(0), Op1,
-                         Op2);
+  return DAG.getNode(N->getOpcode(), SDLoc(N), Op1.getValueType(),
+                     N->getOperand(0), Op1, Op2);
 }
 
 SDValue DAGTypeLegalizer::SoftPromoteHalfRes_SELECT_CC(SDNode *N) {
